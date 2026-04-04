257d88455392dca5dddc401530a831a44680a0cc
YannickZahinda <ymulikuza@gmail.com>
2026-04-04 13:46:52 +0200

    modify input color

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -396,7 +396,7 @@ const Services = () => {
                   <Search className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
                   <Input
                     placeholder="Rechercher un service..."
-                    className="pl-10"
+                    className="pl-10 text-black"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                   />
